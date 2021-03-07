c402ef4a570364043572806a92943d8932e1b4ad
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-03-07 10:45:58 -0300

    Criado novo Modelo

---
+++ microservice-course-liquibase/src/main/java/com/talissonmelo/model/Transaction.java
@@ -0,0 +1,31 @@
+package com.talissonmelo.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "course_id", referencedColumnName = "id")
+    private Course course;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "date_of_issue")
+    private LocalDateTime dateOfIssue;
+}
