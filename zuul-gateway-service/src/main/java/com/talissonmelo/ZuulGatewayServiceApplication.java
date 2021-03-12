3d0a61248a4ca23d9ab265cefc37d0c7e0e5578f
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-03-11 22:13:33 -0300

    Criado  api-gateway

---
+++ zuul-gateway-service/src/main/java/com/talissonmelo/ZuulGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
+	}
+
+}
