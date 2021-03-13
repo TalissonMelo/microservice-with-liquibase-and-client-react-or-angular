1e49a299354fc679763c202b6ad1fe4cd8bd99a4
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-03-12 21:15:33 -0300

    Liberando CORS

--- zuul-gateway-service/src/main/java/com/talissonmelo/ZuulGatewayServiceApplication.java
+++ zuul-gateway-service/src/main/java/com/talissonmelo/ZuulGatewayServiceApplication.java
@@ -2,12 +2,28 @@ package com.talissonmelo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class ZuulGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configurer(){
+		return  new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
