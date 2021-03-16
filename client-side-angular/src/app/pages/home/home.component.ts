3aea51b3f43df7e51972882a90f00bd8f90cacaa
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-03-15 21:30:57 -0300

    Listando cursos e inicializando detalhes e inscrição

--- client-side-angular/src/app/pages/home/home.component.ts
+++ client-side-angular/src/app/pages/home/home.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/model/course';
+import { CourseService } from 'src/app/service/course.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  public courses: Course[] = [];
 
-  constructor() { }
+  constructor(private service: CourseService) { }
 
   ngOnInit(): void {
+    this.service.findAllCourses().subscribe((res) => this.courses = res);
   }
 
+  detail():void {
+    console.log("Detail");
+  }
+
+  register(): void {
+    console.log("Register")
+  }
 }
